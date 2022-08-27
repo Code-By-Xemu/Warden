5ca91d576c1641aa815a84020c016d406ab0ee5a
Magnus K <74688700+codebyxemu@users.noreply.github.com>
2022-08-27 17:06:47 +0200

    Initial commit

---
+++ src/main/java/me/xemu/warden/WardenPlugin.java
@@ -0,0 +1,17 @@
+package me.xemu.warden;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WardenPlugin extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		// Plugin startup logic
+
+	}
+
+	@Override
+	public void onDisable() {
+		// Plugin shutdown logic
+	}
+}
